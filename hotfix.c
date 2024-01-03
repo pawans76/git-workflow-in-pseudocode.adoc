3729dfe7f942154283804bd3f4adebe3222c796d
Pawan Kumar <143925381+pawans76@users.noreply.github.com>
2024-01-03 08:48:27 +0100

    Add files via upload

---
+++ hotfix.c
@@ -0,0 +1,7 @@
+
+pseudo code
+{
+Initial Function()
+{
+  //added hotfix for fixing minor issue 
+}
---
+++ main.c
@@ -0,0 +1,14 @@
+pseude code
+{
+Initial Function()
+{
+	//Sample Code
+}
+}
+feature1{
+	//sample code of feature1
+}
+feature2{
+	//added some code forin feature2 method to fix bug
+	//sample code of feature2
+}
